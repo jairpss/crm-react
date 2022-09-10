4e1da65c0b059d5e80090bc55becc34d980b08ee
Jair Partida <jairpartida1@gmail.com>
2022-09-09 23:52:46 -0600

    adding Link component instead of a

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom/'
+import { Outlet, Link } from 'react-router-dom/'
 
 const Layout = () => {
   return (
@@ -6,15 +6,15 @@ const Layout = () => {
         <div className="md:w-1/4 bg-slate-600 px-5 py-10">
             <h2 className="text-4xl font-bold text-center text-white font-inter">CRM Customers</h2>
             <nav className="mt-10">
-                <a 
+                <Link 
                     className="text-white text-2xl block mt-2 font-inter hover:text-blue-300"
-                    href="/customers"
-                >Customers</a>
+                    to="/customers"
+                >Customers</Link>
 
-                <a 
+                <Link 
                     className="text-white text-2xl block mt-2 font-inter hover:text-blue-300"
-                    href="/customers/new"
-                >New Customers</a>
+                    to="/customers/new"
+                >New Customers</Link>
             </nav>
         </div>
         <div className="md:w-3/4">
